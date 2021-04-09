0b1c020c19ee5ba1684867b91568aacc45d39da4
Eliab_Evora <56361891+EvoraHn@users.noreply.github.com>
2021-04-08 22:25:14 -0600

    footer y css para proyecto

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,32 @@
+//import './footer.css';
+import '../css/galeria.css';
+import '../css/prueba2.css';
+
+function Footer(){
+    return (
+        
+        <footer >
+
+            <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"/>
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+
+           <nav>
+                <ul>
+                    <li class="media"><i class="fab fa-facebook"></i><a href="https://www.facebook.com/munisannicolas/">Facebook</a></li>
+                    <li class="media"><i class="fab fa-instagram"></i><a href="https://www.instagram.com/explore/locations/265774154/san-nicolas-santa-barbara-honduras/?hl=es">Instagram</a></li>
+                    <li class="media"><i class="fab fa-twitter"></i><a href="https://twitter.com/sannicolashn">Twitter</a></li>
+                </ul>
+            </nav>
+
+            <section>
+                <p>Todos los derechos reservados &copy; 2020</p>
+            </section>
+
+        </footer>
+
+    );
+  }
+  
+  export default Footer;
+  
---
+++ src/Components/Galeria/Galeria.js
@@ -0,0 +1,165 @@
+import '../css/galeria.css';
+import '../css/prueba2.css';
+
+function Galeria(){
+    return (
+        
+        <div>
+
+            <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"/>
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+
+            <section class="galeria">
+                <section class="tarjeta">
+                    <a href="../../Tratadas/g (1).jpg">
+                        <img src="../../assets/g (1).jpg" alt="Imagen 1"/>
+                        <p>Imagen 1</p>
+                    </a>
+                </section>
+                <section class="tarjeta">
+                    <a href="Tratadas/g (2).jpg">
+                        <img src="assets/g (2).jpg" alt="Imagen 2"/>
+                        <p>Imagen 2</p>
+                    </a>
+                </section>
+                <section class="tarjeta">
+                    <a href="Tratadas/g (3).jpg">
+                        <img src="assets/g (3).jpg" alt="Imagen 3"/>
+                        <p>Imagen 3</p>
+                    </a>
+                </section>
+                <section class="tarjeta">
+                    <a href="Tratadas/g (4).jpg">
+                        <img src="assets/g (4).jpg" alt="Imagen 4"/>
+                        <p>Imagen 4</p>
+                    </a>
+                </section>
+                <section class="tarjeta">
+                    <a href="Tratadas/g (5).jpg">
+                        <img src="assets/g (5).jpg" alt="Imagen 5"/>
+                        <p>Imagen 5</p>
+                    </a>
+                </section>
+                <section class="tarjeta">
+                    <a href="Tratadas/g (6).jpg">
+                        <img src="assets/g (6).jpg" alt="Imagen 6"/>
+                        <p>Imagen 6</p>
+                    </a>
+                </section>
+                <section class="tarjeta">
+                    <a href="Tratadas/g (7).jpg">
+                        <img src="assets/g (7).jpg" alt="Imagen 7"/>
+                        <p>Imagen 7</p>
+                    </a>
+                </section>
+                <section class="tarjeta">
+                    <a href="Tratadas/g (8).jpg">
+                        <img src="assets/g (8).jpg" alt="Imagen 8"/>
+                        <p>Imagen 8</p>
+                    </a>
+                </section>
+                <section class="tarjeta">
+                    <a href="Tratadas/g (9).jpg">
+                        <img src="assets/g (9).jpg" alt="Imagen 9"/>
+                        <p>Imagen 9</p>
+                    </a>
+                </section>
+                <section class="tarjeta">
+                    <a href="Tratadas/g (10).jpg">
+                        <img src="assets/g (10).jpg" alt="Imagen 10"/>
+                        <p>Imagen 10</p>
+                    </a>
+                </section>
+                <section class="tarjeta">
+                    <a href="Tratadas/g (11).jpg">
+                        <img src="assets/g (11).jpg" alt="Imagen 11"/>
+                        <p>Imagen 11</p>
+                    </a>
+                </section>
+                <section class="tarjeta">
+                    <a href="Tratadas/g (12).jpg">
+                        <img src="assets/g (12).jpg" alt="Imagen 12"/>
+                        <p>Imagen 12</p>
+                    </a>
+                </section>
+                <section class="tarjeta">
+                    <a href="Tratadas/g (14).jpg">
+                        <img src="assets/g (14).jpg" alt="Imagen 14"/>
+                        <p>Imagen 14</p>
+                    </a>
+                </section>
+                <section class="tarjeta">
+                    <a href="Tratadas/g (15).jpg">
+                        <img src="assets/g (15).jpg" alt="Imagen 15"/>
+                        <p>Imagen 15</p>
+                    </a>
+                </section>
+                <section class="tarjeta">
+                    <a href="Tratadas/g (16).jpg">
+                        <img src="assets/g (16).jpg" alt="Imagen 16"/>
+                        <p>Imagen 16</p>
+                    </a>
+                </section>
+                <section class="tarjeta">
+                    <a href="Tratadas/g (17).jpg">
+                        <img src="assets/g (17).jpg" alt="Imagen 17"/>
+                        <p>Imagen 17</p>
+                    </a>
+                </section>
+                <section class="tarjeta">
+                    <a href="Tratadas/g (18).jpg">
+                        <img src="assets/g (18).jpg" alt="Imagen 18"/>
+                        <p>Imagen 18</p>
+                    </a>
+                </section>
+                <section class="tarjeta">
+                    <a href="Tratadas/g (19).jpg">
+                        <img src="assets/g (19).jpg" alt="Imagen 19"/>
+                        <p>Imagen 19</p>
+                    </a>
+                </section>
+                <section class="tarjeta">
+                    <a href="Tratadas/g (20).jpg">
+                        <img src="assets/g (20).jpg" alt="Imagen 20"/>
+                        <p>Imagen 20</p>
+                    </a>
+                </section>
+                <section class="tarjeta">
+                    <a href="Tratadas/g (21).jpg">
+                        <img src="assets/g (21).jpg" alt="Imagen 21"/>
+                        <p>Imagen 21</p>
+                    </a>
+                </section>
+                <section class="tarjeta">
+                    <a href="Tratadas/g (22).jpg">
+                        <img src="assets/g (22).jpg" alt="Imagen 22"/>
+                        <p>Imagen 22</p>
+                    </a>
+                </section>
+                <section class="tarjeta">
+                    <a href="Tratadas/g (23).jpg">
+                    <img src="assets/g (23).jpg" alt="Imagen 23"/>
+                    <p>Imagen 23</p>
+                    </a>
+                </section>
+                <section class="tarjeta">
+                    <a href="Tratadas/g (24).jpg">
+                        <img src="assets/g (24).jpg" alt="Imagen 24"/>
+                        <p>Imagen 24</p>
+                    </a>
+                </section>
+                <section class="tarjeta">
+                    <a href="Tratadas/g (25).jpg">
+                        <img src="assets/g (25).jpg" alt="Imagen 25"/>
+                        <p>Imagen 25</p>
+                    </a>
+                </section>
+            </section>
+        </div>
+
+    );
+  }
+  
+  export default Galeria;
+  
