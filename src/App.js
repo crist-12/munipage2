9786e193cd822b34d1d362a2d69018dace89b2c6
Eliab_Evora <56361891+EvoraHn@users.noreply.github.com>
2021-04-08 22:51:04 -0600

    Galeria en js con react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import Footer from './Components/Footer/Footer'
 import Galeria from './Components/Galeria/Galeria'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-
       <Galeria></Galeria>
       <Footer></Footer>
     </div>
--- src/Components/Galeria/Galeria.js
+++ src/Components/Galeria/Galeria.js
@@ -1,5 +1,55 @@
 import '../css/galeria.css';
 import '../css/prueba2.css';
+import Imagen1  from '../../assets/g (1).jpg'
+import Tratada1  from '../../Tratadas/g (1).jpg'
+import Imagen2  from '../../assets/g (2).jpg'
+import Tratada2  from '../../Tratadas/g (2).jpg'
+import Imagen3  from '../../assets/g (3).jpg'
+import Tratada3  from '../../Tratadas/g (3).jpg'
+import Imagen4  from '../../assets/g (4).jpg'
+import Tratada4  from '../../Tratadas/g (4).jpg'
+import Imagen5  from '../../assets/g (5).jpg'
+import Tratada5  from '../../Tratadas/g (5).jpg'
+import Imagen6  from '../../assets/g (6).jpg'
+import Tratada6  from '../../Tratadas/g (6).jpg'
+import Imagen7  from '../../assets/g (7).jpg'
+import Tratada7  from '../../Tratadas/g (7).jpg'
+import Imagen8  from '../../assets/g (8).jpg'
+import Tratada8  from '../../Tratadas/g (8).jpg'
+import Imagen9  from '../../assets/g (9).jpg'
+import Tratada9  from '../../Tratadas/g (9).jpg'
+import Imagen10  from '../../assets/g (10).jpg'
+import Tratada10  from '../../Tratadas/g (10).jpg'
+import Imagen11  from '../../assets/g (11).jpg'
+import Tratada11  from '../../Tratadas/g (11).jpg'
+import Imagen12  from '../../assets/g (12).jpg'
+import Tratada12  from '../../Tratadas/g (12).jpg'
+import Imagen13  from '../../assets/g (13).jpg'
+import Tratada13  from '../../Tratadas/g (13).jpg'
+import Imagen14  from '../../assets/g (14).jpg'
+import Tratada14  from '../../Tratadas/g (14).jpg'
+import Imagen15  from '../../assets/g (15).jpg'
+import Tratada15  from '../../Tratadas/g (15).jpg'
+import Imagen16  from '../../assets/g (16).jpg'
+import Tratada16  from '../../Tratadas/g (16).jpg'
+import Imagen17  from '../../assets/g (17).jpg'
+import Tratada17  from '../../Tratadas/g (17).jpg'
+import Imagen18  from '../../assets/g (18).jpg'
+import Tratada18  from '../../Tratadas/g (18).jpg'
+import Imagen19  from '../../assets/g (19).jpg'
+import Tratada19  from '../../Tratadas/g (19).jpg'
+import Imagen20  from '../../assets/g (20).jpg'
+import Tratada20  from '../../Tratadas/g (20).jpg'
+import Imagen21  from '../../assets/g (21).jpg'
+import Tratada21  from '../../Tratadas/g (1).jpg'
+import Imagen22  from '../../assets/g (22).jpg'
+import Tratada22  from '../../Tratadas/g (2).jpg'
+import Imagen23  from '../../assets/g (23).jpg'
+import Tratada23  from '../../Tratadas/g (3).jpg'
+import Imagen24  from '../../assets/g (24).jpg'
+import Tratada24  from '../../Tratadas/g (24).jpg'
+import Imagen25  from '../../assets/g (25).jpg'
+import Tratada25  from '../../Tratadas/g (25).jpg'
 
 function Galeria(){
     return (
@@ -9,153 +59,157 @@ function Galeria(){
             <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"/>
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous"/>
             <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-
+            <main>
             <section class="galeria">
                 <section class="tarjeta">
-                    <a href="../../Tratadas/g (1).jpg">
-                        <img src="../../assets/g (1).jpg" alt="Imagen 1"/>
-                        <p>Imagen 1</p>
+                    <a href={Tratada1}>
+                        <img src={Imagen1} alt="Imagen"/>
+                        
                     </a>
                 </section>
                 <section class="tarjeta">
-                    <a href="Tratadas/g (2).jpg">
-                        <img src="assets/g (2).jpg" alt="Imagen 2"/>
-                        <p>Imagen 2</p>
+                    <a href={Tratada2}>
+                        <img src={Imagen2} alt="Imagen"/>
+                        
                     </a>
                 </section>
                 <section class="tarjeta">
-                    <a href="Tratadas/g (3).jpg">
-                        <img src="assets/g (3).jpg" alt="Imagen 3"/>
-                        <p>Imagen 3</p>
+                    <a href={Tratada3}>
+                        <img src={Imagen3} alt="Imagen"/>
+                        
                     </a>
                 </section>
                 <section class="tarjeta">
-                    <a href="Tratadas/g (4).jpg">
-                        <img src="assets/g (4).jpg" alt="Imagen 4"/>
-                        <p>Imagen 4</p>
+                    <a href={Tratada4}>
+                        <img src={Imagen4} alt="Imagen"/>
+                        
                     </a>
                 </section>
                 <section class="tarjeta">
-                    <a href="Tratadas/g (5).jpg">
-                        <img src="assets/g (5).jpg" alt="Imagen 5"/>
-                        <p>Imagen 5</p>
+                    <a href={Tratada5}>
+                        <img src={Imagen5} alt="Imagen"/>
+                        
                     </a>
                 </section>
                 <section class="tarjeta">
-                    <a href="Tratadas/g (6).jpg">
-                        <img src="assets/g (6).jpg" alt="Imagen 6"/>
-                        <p>Imagen 6</p>
+                    <a href={Tratada6}>
+                        <img src={Imagen6} alt="Imagen"/>
+                        
                     </a>
                 </section>
                 <section class="tarjeta">
-                    <a href="Tratadas/g (7).jpg">
-                        <img src="assets/g (7).jpg" alt="Imagen 7"/>
-                        <p>Imagen 7</p>
+                    <a href={Tratada7}>
+                        <img src={Imagen7} alt="Imagen"/>
+                        
                     </a>
                 </section>
                 <section class="tarjeta">
-                    <a href="Tratadas/g (8).jpg">
-                        <img src="assets/g (8).jpg" alt="Imagen 8"/>
-                        <p>Imagen 8</p>
+                    <a href={Tratada8}>
+                        <img src={Imagen8} alt="Imagen"/>
+                        
                     </a>
                 </section>
                 <section class="tarjeta">
-                    <a href="Tratadas/g (9).jpg">
-                        <img src="assets/g (9).jpg" alt="Imagen 9"/>
-                        <p>Imagen 9</p>
+                    <a href={Tratada9}>
+                        <img src={Imagen9} alt="Imagen"/>
+                        
                     </a>
                 </section>
                 <section class="tarjeta">
-                    <a href="Tratadas/g (10).jpg">
-                        <img src="assets/g (10).jpg" alt="Imagen 10"/>
-                        <p>Imagen 10</p>
+                    <a href={Tratada10}>
+                        <img src={Imagen10} alt="Imagen"/>
+                        
                     </a>
                 </section>
                 <section class="tarjeta">
-                    <a href="Tratadas/g (11).jpg">
-                        <img src="assets/g (11).jpg" alt="Imagen 11"/>
-                        <p>Imagen 11</p>
+                    <a href={Tratada11}>
+                        <img src={Imagen11} alt="Imagen"/>
+                        
                     </a>
                 </section>
                 <section class="tarjeta">
-                    <a href="Tratadas/g (12).jpg">
-                        <img src="assets/g (12).jpg" alt="Imagen 12"/>
-                        <p>Imagen 12</p>
+                    <a href={Tratada12}>
+                        <img src={Imagen12} alt="Imagen"/>
+                        
                     </a>
                 </section>
                 <section class="tarjeta">
-                    <a href="Tratadas/g (14).jpg">
-                        <img src="assets/g (14).jpg" alt="Imagen 14"/>
-                        <p>Imagen 14</p>
+                    <a href={Tratada13}>
+                        <img src={Imagen13} alt="Imagen"/>
+                        
                     </a>
                 </section>
                 <section class="tarjeta">
-                    <a href="Tratadas/g (15).jpg">
-                        <img src="assets/g (15).jpg" alt="Imagen 15"/>
-                        <p>Imagen 15</p>
+                    <a href={Tratada14}>
+                        <img src={Imagen14} alt="Imagen"/>
+                        
                     </a>
                 </section>
                 <section class="tarjeta">
-                    <a href="Tratadas/g (16).jpg">
-                        <img src="assets/g (16).jpg" alt="Imagen 16"/>
-                        <p>Imagen 16</p>
+                    <a href={Tratada15}>
+                        <img src={Imagen15} alt="Imagen"/>
+                        
                     </a>
                 </section>
                 <section class="tarjeta">
-                    <a href="Tratadas/g (17).jpg">
-                        <img src="assets/g (17).jpg" alt="Imagen 17"/>
-                        <p>Imagen 17</p>
+                    <a href={Tratada16}>
+                        <img src={Imagen16} alt="Imagen"/>
+                        
                     </a>
                 </section>
                 <section class="tarjeta">
-                    <a href="Tratadas/g (18).jpg">
-                        <img src="assets/g (18).jpg" alt="Imagen 18"/>
-                        <p>Imagen 18</p>
+                    <a href={Tratada17}>
+                        <img src={Imagen17} alt="Imagen"/>
+                        
                     </a>
                 </section>
                 <section class="tarjeta">
-                    <a href="Tratadas/g (19).jpg">
-                        <img src="assets/g (19).jpg" alt="Imagen 19"/>
-                        <p>Imagen 19</p>
+                    <a href={Tratada18}>
+                        <img src={Imagen18} alt="Imagen"/>
+                        
                     </a>
                 </section>
                 <section class="tarjeta">
-                    <a href="Tratadas/g (20).jpg">
-                        <img src="assets/g (20).jpg" alt="Imagen 20"/>
-                        <p>Imagen 20</p>
+                    <a href={Tratada19}>
+                        <img src={Imagen19} alt="Imagen"/>
+                        
                     </a>
                 </section>
                 <section class="tarjeta">
-                    <a href="Tratadas/g (21).jpg">
-                        <img src="assets/g (21).jpg" alt="Imagen 21"/>
-                        <p>Imagen 21</p>
+                    <a href={Tratada20}>
+                        <img src={Imagen20} alt="Imagen"/>
+                        
                     </a>
                 </section>
                 <section class="tarjeta">
-                    <a href="Tratadas/g (22).jpg">
-                        <img src="assets/g (22).jpg" alt="Imagen 22"/>
-                        <p>Imagen 22</p>
+                    <a href={Tratada21}>
+                        <img src={Imagen21} alt="Imagen"/>
+                        
                     </a>
                 </section>
                 <section class="tarjeta">
-                    <a href="Tratadas/g (23).jpg">
-                    <img src="assets/g (23).jpg" alt="Imagen 23"/>
-                    <p>Imagen 23</p>
+                    <a href={Tratada22}>
+                        <img src={Imagen22} alt="Imagen"/>
+                        
                     </a>
                 </section>
                 <section class="tarjeta">
-                    <a href="Tratadas/g (24).jpg">
-                        <img src="assets/g (24).jpg" alt="Imagen 24"/>
-                        <p>Imagen 24</p>
+                    <a href={Tratada23}>
+                        <img src={Imagen23} alt="Imagen"/>
+                        
                     </a>
                 </section>
                 <section class="tarjeta">
-                    <a href="Tratadas/g (25).jpg">
-                        <img src="assets/g (25).jpg" alt="Imagen 25"/>
-                        <p>Imagen 25</p>
+                    <a href={Tratada24}>
+                        <img src={Imagen24} alt="Imagen"/>
+                        
                     </a>
                 </section>
+                
+                
+                
             </section>
+            </main>
         </div>
 
     );
